3b4af3daf4b4cf17177c1db1313e118b853a2eeb
maowei <ksmwly@gmail.com>
2017-03-18 19:33:24 +0800

    init

---
+++ src/main/java/com/meybosoft/saas/web/dao/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.meybosoft.saas.web.dao;
+
+import com.meybosoft.saas.web.domain.User;
+
+import cn.org.disco.boot.platform.dao.BaseRepository;
+
+public interface IUserRepository extends BaseRepository<User, Long> {
+
+}
